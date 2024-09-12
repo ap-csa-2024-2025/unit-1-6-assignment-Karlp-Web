0c823499935689a78f1d4835ab756d26a9fefa76
Karlp-Web <Karlenportillo1@gmail.com>
2024-09-12 11:21:55 -0700

    yomomma

--- Problem3.java
+++ Problem3.java
@@ -1,1 +1,20 @@
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    double num = 67.3424;
 
+    int betterNum = (int) (num * 1000);//will bring the decimals out 
+
+    System.out.println(betterNum % 10); 
+
+    betterNum = betterNum / 10;
+
+    System.out.println(betterNum % 10);
+
+    betterNum = betterNum / 10;
+
+    System.out.println(betterNum % 10);
+    
+  }
+}
