92a99c56ffd32108ce7d7254cc3695535a4e3440
Karlp-Web <Karlenportillo1@gmail.com>
2024-09-12 11:08:09 -0700

    yomomma

--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,18 @@
+public class Problem2
+{
+  public static void main(String[] args)
+  {
+    double x = 57.3934;
+
+    double y = 22.5211;
+
+    int val1 = (int)(x + 0.5);
+
+    int val2 = (int)(y + 0.5);
+
+    int ans = val1 + val2;
+
+    System.out.println(ans);
+  }
+}
 
